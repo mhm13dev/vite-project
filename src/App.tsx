89f8c45f55c478b94f14240946d922631e57fcd0
Mubashir Hassan <mhm13dev@gmail.com>
2024-08-22 00:42:06 +0500

    implemented autocomplete box

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,73 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Autocomplete, Box, TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selected, setSelected] = useState(defaultOption);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Box width={400}>
+      <Autocomplete
+        fullWidth
+        options={options}
+        getOptionLabel={(option) => option.label}
+        value={selected}
+        onChange={(_, newValue) => {
+          setSelected(newValue ?? defaultOption);
+        }}
+        renderInput={(params) => (
+          <TextField {...params} label="Days of the week" fullWidth />
+        )}
+      />
+
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          setSelected(
+            options.find((option) => option.label === 'Friday') ??
+              defaultOption,
+          );
+        }}
+      >
+        Choose Friday
+      </Button>
+    </Box>
   );
 }
 
 export default App;
+
+const options = [
+  {
+    label: 'Monday',
+    value: 1,
+  },
+  {
+    label: 'Tuesday',
+    value: 2,
+  },
+  {
+    label: 'Wednesday',
+    value: 3,
+  },
+  {
+    label: 'Thursday',
+    value: 4,
+  },
+  {
+    label: 'Friday',
+    value: 5,
+  },
+  {
+    label: 'Saturday',
+    value: 6,
+  },
+  {
+    label: 'Sunday',
+    value: 7,
+  },
+];
+
+const defaultOption = options.find((option) => option.value === 1)!;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.tsx';
 import './index.css';
 import '@fontsource/roboto/300.css';
@@ -7,8 +9,19 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#ffffff',
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
